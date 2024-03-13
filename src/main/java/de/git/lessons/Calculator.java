178094c70b63f056e03b8082032c0975cb350af3
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-03-13 21:18:17 +0100

    #ait-git-lessons method multiplication was created

--- src/main/java/de/git/lessons/Calculator.java
+++ src/main/java/de/git/lessons/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public double sum(double numberOne, double numberTwo){
         return numberOne + numberTwo;
     }
+
+    public double multiplication(double numberOne, double numberTwo){
+        return numberOne*numberTwo;
+    }
 }
